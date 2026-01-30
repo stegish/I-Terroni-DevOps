23ec2cae1c7d7d9f3704b65d137f0ce51e18f42c
stegish <fantinato.michael@gmail.com>
2026-01-30 15:09:11 +0100

    added itu-minitwit original code

---
+++ control.sh
@@ -0,0 +1,24 @@
+if [ $1 = "init" ]; then
+
+    if [ -f "/tmp/minitwit.db" ]; then 
+        echo "Database already exists."
+        exit 1
+    fi
+    echo "Putting a database to /tmp/minitwit.db..."
+    python -c"from minitwit import init_db;init_db()"
+elif [ $1 = "startprod" ]; then
+     echo "Starting minitwit with production webserver..."
+     nohup $HOME/.local/bin/gunicorn --workers 4 --timeout 120 --bind 0.0.0.0:5000 minitwit:app > /tmp/out.log 2>&1 &
+elif [ $1 = "start" ]; then
+    echo "Starting minitwit..."
+    nohup `which python` minitwit.py > /tmp/out.log 2>&1 &
+elif [ $1 = "stop" ]; then
+    echo "Stopping minitwit..."
+    pkill -f minitwit
+elif [ $1 = "inspectdb" ]; then
+    ./flag_tool -i | less
+elif [ $1 = "flag" ]; then
+    ./flag_tool "$@"
+else
+  echo "I do not know this command..."
+fi
