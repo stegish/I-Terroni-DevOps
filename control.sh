06ab1c79752fffc7c27e340cd6c4fbf1d6f9da5e
alettablur <gbmtt10@gmail.com>
2026-02-05 17:30:32 +0000

    Refactoring control.sh

--- control.sh
+++ control.sh
@@ -1,4 +1,4 @@
-if [ $1 = "init" ]; then
+if [ "$1" = "init" ]; then  #REFACTORING "$1"
 
     if [ -f "/tmp/minitwit.db" ]; then 
         echo "Database already exists."
@@ -6,18 +6,18 @@ if [ $1 = "init" ]; then
     fi
     echo "Putting a database to /tmp/minitwit.db..."
     python -c"from minitwit import init_db;init_db()"
-elif [ $1 = "startprod" ]; then
+elif [ "$1" = "startprod" ]; then   #REFACTORING "$1"
      echo "Starting minitwit with production webserver..."
-     nohup $HOME/.local/bin/gunicorn --workers 4 --timeout 120 --bind 0.0.0.0:5000 minitwit:app > /tmp/out.log 2>&1 &
-elif [ $1 = "start" ]; then
+     nohup "$HOME"/.local/bin/gunicorn --workers 4 --timeout 120 --bind 0.0.0.0:5000 minitwit:app > /tmp/out.log 2>&1 & #REFACTORING "$HOME"
+elif [ "$1" = "start" ]; then   #REFACTORING "$1"
     echo "Starting minitwit..."
-    nohup `which python` minitwit.py > /tmp/out.log 2>&1 &
-elif [ $1 = "stop" ]; then
+    nohup $(which python) minitwit.py > /tmp/out.log 2>&1 & #REFACTORING $()
+elif [ "$1" = "stop" ]; then    #REFACTORING "$1"
     echo "Stopping minitwit..."
     pkill -f minitwit
-elif [ $1 = "inspectdb" ]; then
+elif [ "$1" = "inspectdb" ]; then   #REFACTORING "$1"
     ./flag_tool -i | less
-elif [ $1 = "flag" ]; then
+elif [ "$1" = "flag" ]; then    #REFACTORING "$1"
     ./flag_tool "$@"
 else
   echo "I do not know this command..."
