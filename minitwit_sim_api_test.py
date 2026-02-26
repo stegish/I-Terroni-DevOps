92b2db914451aef12edf9509ea1e68bebc549c31
stegish <fantinato.michael@gmail.com>
2026-02-26 17:00:30 +0100

    changed tests port

--- minitwit_sim_api_test.py
+++ minitwit_sim_api_test.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from contextlib import closing
 
 
-BASE_URL = 'http://localhost:8080/'
+BASE_URL = 'http://localhost:5000/'
 DATABASE = "tmp/minitwit.db"
 USERNAME = 'simulator'
 PWD = 'super_safe!'
