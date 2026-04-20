b6c56b26f74917d2466c326ff5ee721161d911a0
Beni <sz.benu@gmail.com>
2026-04-20 17:06:57 +0200

    query optimalization

--- db.py
+++ db.py
@@ -4,7 +4,13 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 DATABASE_URI = os.environ.get("DATABASE_URL", "")
-engine = create_engine(DATABASE_URI)
+engine = create_engine(
+    DATABASE_URI,
+    pool_size=10,
+    max_overflow=20,
+    pool_pre_ping=True,
+    pool_recycle=3600
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
