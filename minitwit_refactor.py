e009e7c6b82cda990a5a229cee572c97fd2bb147
Vincenzo Sabino <vins@itu.dk>
2026-05-17 21:06:55 +0200

    code review

--- minitwit_refactor.py
+++ minitwit_refactor.py
@@ -68,7 +68,7 @@ def gravatar_url(email, size=80):
     (semgrep, bandit, Codacy) don't flag this as a weak signature.
     """
     email_bytes = email.strip().lower().encode("utf-8")
-    email_hash = md5(email_bytes, usedforsecurity=False).hexdigest()  # nosec B324  # nosemgrep
+    email_hash = md5(email_bytes, usedforsecurity=False).hexdigest() # nosec B324  # nosemgrep
     return "http://www.gravatar.com/avatar/%s?d=identicon&s=%d" % (email_hash, size)
 
 
